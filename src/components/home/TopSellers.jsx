32018278963908fe398cdcccc988d6d0a1a79a65
Cam <cameronwatkins46@gmail.com>
2024-10-05 17:57:40 -0500

    returned top sellers back to normal forgot to change branches

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
-import axios from "axios";
 
 const TopSellers = () => {
-  const [sellers, setSellers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchTopSellers() {
-      try {
-        const { data } = await axios.get(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
-        );
-        setSellers(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data", error);
-        setLoading(false);
-      }
-    }
-    fetchTopSellers();
-  }, []);
-
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -35,36 +15,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-            {loading
-                ? new Array(12).fill(0).map((_, index) => (
-                    <li key={index}>
-                      <div className="author_list_pp top-sellers__author--image-skeleton">
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className="author_list_info">
-                        <p className="top-sellers__author-info--skeleton"></p>
-                        <p className="top-sellers__author--price-skeleton"></p>
-                      </div>
-                    </li>
-                  ))
-                : sellers.length > 1 &&
-                  sellers.map((sellers) => (
-                <li key={sellers.id}>
+              {new Array(12).fill(0).map((_, index) => (
+                <li key={index}>
                   <div className="author_list_pp">
-                    <Link to={`/author/${sellers.id}`}>
+                    <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={sellers.authorImage}
+                        src={AuthorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to={`/author/${sellers.authorId}`}>
-                      {sellers.authorName}
-                    </Link>
-                    <span>{sellers.price} ETH</span>
+                    <Link to="/author">Monica Lucas</Link>
+                    <span>2.1 ETH</span>
                   </div>
                 </li>
               ))}
